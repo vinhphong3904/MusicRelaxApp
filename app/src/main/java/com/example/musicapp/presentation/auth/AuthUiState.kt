8fb28e80e6de5023a78ea1ede29d00f6397f8901
vinhphong3904 <0306221152@caothang.edu.vn>
2026-01-26 08:58:41 +0700

    update

--- app/src/main/java/com/example/musicapp/presentation/auth/AuthUiState.kt
+++ app/src/main/java/com/example/musicapp/presentation/auth/AuthUiState.kt
@@ -1,13 +1,13 @@
 package com.example.musicapp.presentation.auth
 
-import com.example.musicapp.data.model.UserModel
+import com.example.musicapp.data.model.UserDto
 
 sealed class AuthUiState {
     object Idle : AuthUiState()
     object Loading : AuthUiState()
 
     data class Success(
-        val user: UserModel,
+        val user: UserDto,
         val from: AuthAction   // login hay register
     ) : AuthUiState()
 
